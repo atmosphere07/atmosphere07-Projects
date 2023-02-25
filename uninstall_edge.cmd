ce810bc57edc4a0314fb0acf483015deea8009da
atmosphere07 <123338916+atmosphere07@users.noreply.github.com>
2023-02-25 17:32:36 +0800

    Delete uninstall_edge.cmd

--- uninstall_edge.cmd
+++
@@ -1,7 +0,0 @@
-echo uninstall edge
-
-cd C:\Program Files (x86)\Microsoft\Edge\Application\110.0.1587.50\Installer
-
-.\setup.exe -uninstall -system-level -verbose-logging -force-uninstall
-
-echo done!
