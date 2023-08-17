3ce00ff8d9c2e777982407f20ab11e3c7f898c28
atmosphere07 <123338916+atmosphere07@users.noreply.github.com>
2023-08-18 00:46:56 +0800

    Add files via upload

--- hash-en-sh.py
+++ hash-en-sh.py
@@ -1,25 +1,27 @@
 import hashlib
 import sys
 
-help  ="""
-hash-en-sh by atmosphere07 v1.0
-see https://github.com/atmosphere07/atmosphere07-Projects
-hash-en-sh [mod] [filenname]
-mod:
-    -md5       md5
-    -sha1      sha1
-    -sha224    sha224
-    -sha256    sha256
-    -sha384    sha384
-    -sha512    sha512
+help2 = """hash-en-sh v1.5
+Copyright (c) 2022-2023 by atmosphere07
+hash-en-sh [OPINION] [FILENAME]
+hash-em-sh --help show all help"""
+help  ="""hash-en-sh v1.5
+Copyright (c) 2022-2023 by atmosphere07
+Homepage: https://github.com/atmosphere07/atmosphere07-Projects
+hash-en-sh [OPINION] [FILENAME]
+Usage:
+    -md5
+    -sha1
+    -sha224
+    -sha256
+    -sha384
+    -sha512
+    -h(--help, -help) you are watching it now"""
+help3 = "helloword"
 
-hash-en-sh -md5 hash-en.py
-d45027a6881d26cf4f6120fd7b126dae
-"""
 
 try:
-    mod = sys.argv[1]
-
+    opinion = sys.argv[1]
     def sha256(filename):
         with open(filename, 'rb') as f:
             data = f.read()
@@ -44,28 +46,33 @@ try:
         with open(filename, 'rb') as f:
             data = f.read()
         return hashlib.sha512(data)
-
-    if mod != "-h":
-        file = sys.argv[2]
-        try:
-            if mod == "-md5":
-                data2 = md5(file)
-            elif mod == "-sha1":
-                data2 = sha1(file)
-            elif mod == "-sha256":
-                data2 = sha256(file)
-            elif mod == "-sha384":
-                data2 = sha384(file)
-            elif mod == "-sha224":
-                data2 = sha224(file)
-            elif mod == "-sha512":
-                data2 = sha256(file)
+    file = sys.argv[2]
+    try:
+        if opinion == "-md5":
+            data2 = md5(file)
             print(data2.hexdigest())
-        except(NameError):
-            print("No mod")
-        except(FileNotFoundError):
-            print("No such file or directory")
-    elif mod == "-h":
-        print(help)
+        elif opinion == "-sha1":
+            data2 = sha1(file)
+            print(data2.hexdigest())
+        elif opinion == "-sha256":
+            data2 = sha256(file)
+            print(data2.hexdigest())
+        elif opinion == "-sha384":
+            data2 = sha384(file)
+            print(data2.hexdigest())
+        elif opinion == "-sha224":
+            data2 = sha224(file)
+            print(data2.hexdigest())
+        elif opinion == "-sha512":
+            data2 = sha256(file)
+            print(data2.hexdigest())
+        elif opinion == "--help":
+            print(help)
+        elif opinion == "-h":
+            print(help)
+        elif opinion == "-help":
+            print(help)
+    except(FileNotFoundError):
+        print("No file named", sys.argv[2], "in this directory")
 except:
-    print(help)
+    print(help2)    
